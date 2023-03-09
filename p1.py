9271517f1ab9f9735bfcda1bb98ca480a0ee6324
Nikhil Sengupta <nikhil.sengupta10@protonmail.com>
2023-03-09 00:37:19 +0000

    Basically done with commented code

--- p1.py
+++ p1.py
@@ -1,5 +1,5 @@
 from matrix import get_transmission_prob_matrix, get_emission_prob_matrix
-from unk import find_infrequent_words
+from unk import find_infrequent_words, replace_infrequent_words_train, replace_infrequent_words_test
 from conllu import parse_incr
 from io import open
 from math import log
@@ -74,9 +74,8 @@ def get_accuracy(tags, emission_matrix, transition_matrix, test_sents):
 	total = 0
 
 	for sent in test_sents:
-		# Get the accuracy for each word in the test sentence
 		predicted_tags = viterbi_algorithm(tags, emission_matrix, transition_matrix, sent)
-
+		# Get the accuracy for each word in the test sentence based on the predicted tag
 		for token_index, token in enumerate(sent):
 			if token['upos'] == predicted_tags[token_index]:
 				correct += 1
@@ -133,10 +132,24 @@ def conllu_corpus(path):
 	sents = list(parse_incr(data_file))
 	return [prune_sentence(sent) for sent in sents]
 
-def run_experiment(lang):
+'''
+Runs the experiment for the given language and unk option
+
+args:
+	lang: the language of the corpus
+	use_unk: whether or not to use <unk> for infrequent words
+'''
+def run_experiment(lang, use_unk):
 	train_sents = conllu_corpus(train_corpus(lang))
 	test_sents = conllu_corpus(test_corpus(lang))
 	tags = set([token['upos'] for sent in train_sents for token in sent])
+
+	if use_unk:
+		# Replace infrequent words with <unk>
+		infrequent_words = find_infrequent_words(train_sents)
+
+		train_sents = replace_infrequent_words_train(train_sents, infrequent_words, lang)
+		test_sents = replace_infrequent_words_test(test_sents, train_sents, infrequent_words, lang)
 
 	# Get the transition and emission matrices
 	transition_matrix = get_transmission_prob_matrix(train_sents)
@@ -156,6 +169,36 @@ def run_experiment(lang):
 		case _:
 			print('Accuracy: {:.2%}'.format(accuracy))
 
+'''
+Throws an error if the arguments are invalid
+'''
+def throw_err():
+	print('ERROR: Invalid argument USAGE: python3 viterbi.py [en|fr|uk|all] [unk]')
+	exit()
+
+'''
+Checks if the given argument for the language is valid
+
+args:
+	arg: the argument for the language
+	run_all: whether or not to run the experiment for all languages
+	lang: the language of the corpus
+
+returns:
+	run_all: whether or not to run the experiment for all languages
+	lang: the language of the corpus
+'''
+def check_valid_lang(arg, run_all, lang):
+	languages = ['en', 'fr', 'uk']
+	# Check if to run all languages
+	if arg == 'all':
+		run_all = True
+	# Check if the language is valid
+	if arg in languages:
+		lang = argv[1]
+	else:
+		throw_err()
+	return run_all, lang
 
 def main():
 	# Add the treebanks to the dictionary
@@ -163,23 +206,35 @@ def main():
 	TREEBANK['fr'] = 'UD_French-Rhapsodie/fr_rhapsodie'
 	TREEBANK['uk'] = 'UD_Ukrainian-IU/uk_iu'
 
-	lang = 'en'
+	lang = ''
 	run_all = False
+	use_unk = False
 
-	# Get the language from the command line (default English)
-	if len(argv) > 1:
-		# Run all languages if 'all' is passed in
-		if argv[1] == 'all':
-			run_all = True
-		else:
-			lang = argv[1]
+	# Check the arguments
+	match len(argv):
+		# No arguments (Default to English)
+		case 1:
+			lang = 'en'
+		# One argument (Language)
+		case 2:
+			run_all, lang = check_valid_lang(argv[1], run_all, lang)
+		# Two arguments (Language and unk)
+		case 3:
+			run_all, lang = check_valid_lang(argv[1], run_all, lang)
+			if argv[2] == 'unk':
+				use_unk = True
+			else:
+				throw_err()
+		# Invalid number of arguments
+		case _:
+			throw_err()
 
 	if run_all:
-		run_experiment('en')
-		run_experiment('fr')
-		run_experiment('uk')
+		run_experiment('en', use_unk)
+		run_experiment('fr', use_unk)
+		run_experiment('uk', use_unk)
 	else:
-		run_experiment(lang)
+		run_experiment(lang, use_unk)
 	
 if __name__ == '__main__':
 	main()
--- unk.py
+++ unk.py
@@ -1,18 +1,60 @@
 from nltk import FreqDist
+import unicodedata
 
 FREQ_THRESHOLD = 2
 
+'''
+Check if a word ends with a suffix
+
+args:
+	word: the word to check
+	suffix: the suffix to check
+
+returns:
+	True if the word ends with the suffix, False otherwise
+'''
 def word_ends_with(word, suffix):
 	suff_len = len(suffix)
 	return word[-suff_len:] == suffix
 
-def word_starts_with(word, prefix):
-	pref_len = len(prefix)
-	return word[:pref_len] == prefix
+'''
+Check if a word is capitalized and not the first word in the sentence
 
-def word_is_capitalized(word):
-	return word[0].isupper()
+args:
+	sent: the sentence the word is in
+	word: the word to check
 
+returns:
+	True if the word is capitalized and not the first word in the sentence, False otherwise
+'''
+def word_is_capitalized(sent, word):
+	return word[0].isupper() and sent[0]['form'] != word
+
+'''
+Check if a word is a Ukrainian upper case word and not the first word in the sentence
+
+args:
+	sent: the sentence the word is in
+	word: the word to check
+
+returns:
+	True if the word is a Ukrainian upper case word, False otherwise	
+'''
+def word_is_capitalized_ukr(sent, word):
+    for letter in word:
+        if letter.isalpha():
+            return unicodedata.name(letter).startswith('CYRILLIC CAPITAL') and sent[0]['form'] != word
+    return False
+
+'''
+Find the infrequent words in the corpus
+
+args:
+	sents: the sentences from the corpus
+
+returns:
+	A list of infrequent words
+'''
 def find_infrequent_words(sents):
 	# Initialize the infrequent words
 	infrequent_words = []
@@ -27,12 +69,172 @@ def find_infrequent_words(sents):
 
 	return infrequent_words
 
-def convert_word_to_unk(train_sents, test_sents):
-	unk_tag = 'UNK'
+'''
+Convert a word to an unknown word tag for English
 
-def replace_infrequent_words(train_sents, infrequent_words):
-	# Replace each infrequent word with the string 'UNK'
+args:
+	sent: the sentence the word is in
+	word: the word to convert
+
+returns:
+	The unknown word tag for the word
+'''
+def convert_word_to_unk_en(sent, word):
+	unk_tag = word
+	if word_ends_with(word, 'ing'):
+		unk_tag = 'UNK-ing'
+	elif word_is_capitalized(sent, word):
+		unk_tag = 'UNK-cap'
+	elif word_ends_with(word, 'ly'):
+		unk_tag = 'UNK-ly'
+	elif word_ends_with(word, 'ble'):
+		unk_tag = 'UNK-ble'
+	elif word_ends_with(word, 'fy'):
+		unk_tag = 'UNK-fy'
+	elif word_ends_with(word, 'ic'):
+		unk_tag = 'UNK-ic'
+	elif word_ends_with(word, 'ous'):
+		unk_tag = 'UNK-ous'
+	elif word_ends_with(word, 'ful'):
+		unk_tag = 'UNK-ful'
+	elif word_ends_with(word, 'ed'):
+		unk_tag = 'UNK-ed'
+	elif word_ends_with(word, 'tion'):
+		unk_tag = 'UNK-tion'
+	return unk_tag
+
+'''
+Convert a word to an unknown word tag for French
+
+args:
+	sent: the sentence the word is in
+	word: the word to convert
+
+returns:
+	The unknown word tag for the word
+'''
+def convert_word_to_unk_fr(sent, word):
+	unk_tag = word
+	if word_is_capitalized(sent, word):
+		unk_tag = 'UNK-cap'
+	elif word_ends_with(word, 'ique'):
+		unk_tag = 'UNK-ique'
+	elif word_ends_with(word, 'iste'):
+		unk_tag = 'UNK-iste'
+	elif word_ends_with(word, 'elle'):
+		unk_tag = 'UNK-elle'
+	elif word_ends_with(word, 'eux'):
+		unk_tag = 'UNK-eux'
+	elif word_ends_with(word, 'er'):
+		unk_tag = 'UNK-er'
+	elif word_ends_with(word, 'eur'):
+		unk_tag = 'UNK-eur'
+	elif word_ends_with(word, 'ble'):
+		unk_tag = 'UNK-ble'
+	elif word_ends_with(word, 'ment'):
+		unk_tag = 'UNK-ment'
+	elif word_ends_with(word, 'tion'):
+		unk_tag = 'UNK-tion'
+	return unk_tag
+
+'''
+Convert a word to an unknown word tag for Ukrainian
+
+args:
+	sent: the sentence the word is in
+	word: the word to convert
+
+returns:
+	The unknown word tag for the word
+'''
+def convert_word_to_unk_uk(sent, word):
+	unk_tag = word
+	if word_is_capitalized_ukr(sent, word):
+		unk_tag = 'UNK-cap'
+	elif word_ends_with(word, 'іст'):
+		unk_tag = 'UNK-іст'
+	elif word_ends_with(word, 'ість'):
+		unk_tag = 'UNK-ість'
+	elif word_ends_with(word, 'ка'):
+		unk_tag = 'UNK-ка'
+	elif word_ends_with(word, 'ий'):
+		unk_tag = 'UNK-ий'
+	elif word_ends_with(word, 'ці'):
+		unk_tag = 'UNK-ці'
+	elif word_ends_with(word, 'ня'):
+		unk_tag = 'UNK-ня'
+	elif word_ends_with(word, 'овий'):
+		unk_tag = 'UNK-овий'
+	elif word_ends_with(word, 'ець'):
+		unk_tag = 'UNK-ець'
+	elif word_ends_with(word, 'ло'):
+		unk_tag = 'UNK-ло'
+	return unk_tag
+
+'''
+Replace the infrequent words in the training set with unknown word tags
+
+args:
+	train_sents: the sentences from the training set
+	infrequent_words: the infrequent words in the corpus
+
+returns:
+	the training set with infrequent words replaced with unknown word tags
+'''
+def replace_infrequent_words_train(train_sents, infrequent_words, lang):
 	for sent in train_sents:
 		for token in sent:
-			if token['form'] in infrequent_words:
-				token['form'] = 'UNK'
+			word = token['form']
+			# Replace the word with an unknown word tag if it is infrequent
+			if word in infrequent_words:
+				match lang:
+					case 'en':
+						token['form'] = convert_word_to_unk_en(sent, word)
+					case 'fr':
+						token['form'] = convert_word_to_unk_fr(sent, word)
+					case 'uk':
+						token['form'] = convert_word_to_unk_uk(sent, word)
+	return train_sents
+
+'''
+Replace the infrequent words in the test set with unknown word tags
+
+args:
+	test_sents: the sentences from the test set
+	train_sents: the sentences from the training set
+	infrequent_words: the infrequent words in the training set
+
+returns:
+	the test set with infrequent words replaced with unknown word tags
+'''
+def replace_infrequent_words_test(test_sents, train_sents, infrequent_words, lang):
+	train_set_words = get_train_set_words(train_sents)
+	for sent in test_sents:
+		for token in sent:
+			word = token['form']
+			# Replace the word with an unknown word tag if it is infrequent or not in the training set
+			if word in infrequent_words or word not in train_set_words:
+				match lang:
+					case 'en':
+						token['form'] = convert_word_to_unk_en(sent, word)
+					case 'fr':
+						token['form'] = convert_word_to_unk_fr(sent, word)
+					case 'uk':
+						token['form'] = convert_word_to_unk_uk(sent, word)
+	return test_sents
+
+'''
+Get the words from the training set
+
+args:
+	train_set: the sentences from the training set
+
+returns:
+	A list of words from the training set
+'''
+def get_train_set_words(train_set):
+	train_set_words = []
+	for sent in train_set:
+		for token in sent:
+			train_set_words.append(token['form'])
+	return train_set_words
